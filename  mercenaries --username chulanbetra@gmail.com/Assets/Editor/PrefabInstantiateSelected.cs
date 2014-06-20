d54e5c38ee45bac754af50a52cbe477aedf54aa7
chulanbetra@gmail.com <chulanbetra@gmail.com@ebe7e6f4-bfd3-fe2a-37a6-faed1fc6aa05>
2014-06-20 22:16:48 +0000

    - instancovanie selectnuteho prefabu ako preview pri klikani do project view

---  mercenaries --username chulanbetra@gmail.com/Assets/Editor/PrefabInstantiateSelected.cs
+++  mercenaries --username chulanbetra@gmail.com/Assets/Editor/PrefabInstantiateSelected.cs
@@ -8,15 +8,34 @@ public class PrefabInstantiateSelected : Editor
 	private SceneEdit m_pSceneEdit;	
 	private Plane m_pXZPlane;
 	private GameObject m_pSelectedObject;	
+	private GameObject m_pPreviewPrefab;	
 	
 	void OnEnable()
 	{
 		Debug.Log("init");
 		m_pSceneEdit = (SceneEdit)target;
 		m_pXZPlane = new Plane(Vector3.up, Vector3.zero);
-		SceneView.onSceneGUIDelegate = SceneUpdate;		
+		SceneView.onSceneGUIDelegate = SceneUpdate;	
+		EditorApplication.update += Update;
 	}	
 	
+	void Update()
+	{						
+		GameObject pNewSelectedObject = Selection.activeObject as GameObject;			
+		if (m_pSelectedObject != pNewSelectedObject && IsPrefab(pNewSelectedObject))
+		{
+			Debug.Log("new preview prefab: "+pNewSelectedObject.name);			
+			if (m_pPreviewPrefab != null)
+			{
+				DestroyImmediate(m_pPreviewPrefab);
+			}
+			m_pSelectedObject = pNewSelectedObject;
+			m_pPreviewPrefab = (GameObject)PrefabUtility.InstantiatePrefab(m_pSelectedObject);
+			m_pPreviewPrefab.transform.position = Vector3.zero;
+		}
+	}
+	
+	// returns true if object is prefab in project view
 	bool IsPrefab(GameObject pGO)
 	{
 		if (pGO)
@@ -25,24 +44,22 @@ public class PrefabInstantiateSelected : Editor
 		}
 		return false;		
 	}
-	
+		
+	// called every frame
 	void SceneUpdate(SceneView pSceneView)
 	{	
-		HandleMouseClick(Event.current);		
-	}
-	
-	void HandleMouseClick(Event e)
-	{	
-		m_pSelectedObject = Selection.activeObject as GameObject;
+		Event e = Event.current;
 		if (e.type == EventType.MouseDown && e.button == 0) 
 		{				
 			if (e.alt)
 			{				
 	            InstantiateSelectedPrefab(e);
+				e.Use();
 			}
-        }
-	}	
+        }		
+	}
 	
+	// instantiate prefab selected in project view and place it in scene view on XZ plane under mouse cursor
 	void InstantiateSelectedPrefab(Event e)
 	{
 		Ray pRay = Camera.current.ScreenPointToRay(new Vector3(e.mousePosition.x, -e.mousePosition.y + Camera.current.pixelHeight));
