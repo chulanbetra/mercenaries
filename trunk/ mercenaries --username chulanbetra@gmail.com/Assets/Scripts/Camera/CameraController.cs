8917c5a4adb44710002a2a33714335db77d6d4c9
chulanbetra@gmail.com <chulanbetra@gmail.com@ebe7e6f4-bfd3-fe2a-37a6-faed1fc6aa05>
2014-08-07 00:38:57 +0000

    - enums script

--- trunk/ mercenaries --username chulanbetra@gmail.com/Assets/Scripts/Camera/CameraController.cs
+++ trunk/ mercenaries --username chulanbetra@gmail.com/Assets/Scripts/Camera/CameraController.cs
@@ -16,8 +16,7 @@ public class CameraController : MonoBehaviour
 	
 	// Use this for initialization
 	void Start () 
-	{
-	
+	{		
 	}
 	
 	// Update is called once per frame
---
+++ trunk/ mercenaries --username chulanbetra@gmail.com/Assets/Scripts/Core/Enums.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+public enum eObjectType
+{		
+	ACTOR = 0,
+	DOOR = 1,
+	CONTAINER = 2,
+	ITEM = 3,		
+}
