25d12e4987569be02035cbe91c7a35cc9f6ec110
chulanbetra@gmail.com <chulanbetra@gmail.com@ebe7e6f4-bfd3-fe2a-37a6-faed1fc6aa05>
2014-08-12 21:59:59 +0000

    - wall editor script - pri zmene wall type sa rotuje prefab

---
+++  mercenaries --username chulanbetra@gmail.com/Assets/Editor/CustomWallEditor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Wall))]
+[CanEditMultipleObjects]
+public class CustomWallEditor : Editor 
+{
+	private SerializedProperty WallType;
+	private eWallType oldWallType;
+	private GameObject activeObject;
+	private bool PropertyChanged = false;
+	
+	void OnEnable()
+	{
+		WallType = serializedObject.FindProperty("WallType");
+		oldWallType = (eWallType)WallType.enumValueIndex;
+		activeObject = Selection.activeGameObject;
+	}
+	
+	public override void OnInspectorGUI() 
+	{
+		serializedObject.Update();			
+		
+		eWallType propertyValue = (eWallType)WallType.enumValueIndex;
+		if (oldWallType != propertyValue)
+		{	
+			oldWallType = propertyValue;
+			WallPropertyChanged();
+			SceneView.RepaintAll();
+		}
+		
+		serializedObject.ApplyModifiedProperties();		
+		DrawDefaultInspector();		
+	}	
+	
+	void WallPropertyChanged()
+	{		
+		Transform pTransform = activeObject.transform;
+		Vector3 vRotation = pTransform.localEulerAngles;
+		if (oldWallType == eWallType.WALL_LEFT)
+		{							
+			pTransform.localEulerAngles = new Vector3(vRotation.x, 0.0f, vRotation.z);
+		}
+		else
+		{			
+			pTransform.localEulerAngles = new Vector3(vRotation.x, 90.0f, vRotation.z);			
+		}	
+	}
+}
