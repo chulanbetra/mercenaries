0aa091dbf115521ff3517c5ad446e7a40c95a25d
chulanbetra@gmail.com <chulanbetra@gmail.com@ebe7e6f4-bfd3-fe2a-37a6-faed1fc6aa05>
2014-08-20 20:58:20 +0000

    - AI node class, index point class

---
+++ trunk/ mercenaries --username chulanbetra@gmail.com/Assets/Scripts/AI/AINode.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum eDirection
+{
+	UP = 0,
+	DOWN = 1,
+	LEFT = 2,
+	RIGHT = 3,
+	UP_LEFT = 4,
+	UP_RIGHT = 5,
+	DOWN_LEFT = 6,
+	DOWN_RIGHT = 7,
+}
+
+[System.Serializable]
+public class AINode
+{	
+	private Point m_vPoint;	
+	private Vector3 m_vPos;
+	
+	// neighbor AI nodes (max 8)
+	public Dictionary<eDirection, AINode> NeighborNodes;	
+	
+	// used in pathfinding
+	public bool Visited;
+	public bool IsInOpenSet;
+	// distance from start node	in pathgraf
+	public int DistanceFromStart;
+	// total distance from start to end through this node
+	public float TotalDistance;
+	// reference to parent node (used when creating shortest path)
+	public AINode ParentNode;
+	
+	// index position of tile (index position in map)
+	public Point IndexPosition
+	{
+		get
+		{
+			return m_vPoint;
+		}
+		
+		set
+		{
+			m_vPoint = value;
+		}
+	}
+	
+	// world position of tile
+	public Vector3 Position
+	{
+		get
+		{
+			return m_vPos;
+		}
+		
+		set
+		{
+			m_vPos = value;
+		}
+	}
+	
+	public AINode(Point vPoint, Vector3 vPos)
+	{
+		this.NeighborNodes = new Dictionary<eDirection, AINode>();
+		this.IndexPosition = vPoint;
+		this.Position = vPos;
+	}
+}
---
+++ trunk/ mercenaries --username chulanbetra@gmail.com/Assets/Scripts/Core/Point.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Point
+{
+	public int X;
+	public int Y;
+	
+	public Point(int x, int y)
+	{
+		X = x;
+		Y = y;
+	}
+}
