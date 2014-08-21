04c858cbb61948c411666d4c0a239c5dae2d07af
chulanbetra@gmail.com <chulanbetra@gmail.com@ebe7e6f4-bfd3-fe2a-37a6-faed1fc6aa05>
2014-08-21 23:29:23 +0000

    - aipathgraf class - aisystem class - point comparer & level class

---
+++  mercenaries --username chulanbetra@gmail.com/Assets/Scripts/AI/AISystem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public sealed class AISystem 
+{
+	// ai system singleton 
+	public static readonly AISystem Instance = new AISystem();	
+	
+	public AISystem() 
+	{	
+		Random.seed = (int)System.DateTime.Now.Ticks;
+	}	
+}
