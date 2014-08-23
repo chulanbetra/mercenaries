72d8b0e39c09919d055a2d830a0f30c6c65966dc
chulanbetra@gmail.com <chulanbetra@gmail.com@ebe7e6f4-bfd3-fe2a-37a6-faed1fc6aa05>
2014-08-23 01:14:26 +0000

    - presun enumov do spolocneho suboru - vytvorenie tilov + draw debug gizmo

---  mercenaries --username chulanbetra@gmail.com/Assets/Scripts/Core/Tile.cs
+++  mercenaries --username chulanbetra@gmail.com/Assets/Scripts/Core/Tile.cs
@@ -1,28 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public enum eTileFlag
-{
-	NONE = 0x00,
-	// flags for moving
-	WALKABLE = 0x01,
-	// wall flags	
-	WALL_LEFT = 0x02,
-	WALL_JUMP_LEFT = 0x04,
-	WALL_DOWN = 0x08,
-	WALL_JUMP_DOWN = 0x10,
-	// open door flags
-	DOOR_LEFT = 0x20,
-	DOOR_RIGHT = 0x40,
-	DOOR_UP = 0x80,
-	DOOR_DOWN = 0x100,
-	// dynamics (actors, destroyable objects etc)
-	OBSTACLE = 0x200,
-	// can be moved through by animation
-	CRAWL_UNDER = 0x400,
-	JUMP_OVER = 0x800,
-}
-
 [System.Serializable]
 // tile class
 public class Tile
